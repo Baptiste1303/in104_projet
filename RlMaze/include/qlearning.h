0e5175a044c41fdf2e29c648715516cc49535b58
MarcChen <chen987415@hotmail.fr>
2023-04-19 22:45:08 +0200

    Déplacement du Q algorithm dans une fonction à part entière

---
+++ RlMaze/include/qlearning.h
@@ -0,0 +1,31 @@
+#ifndef QLEARNING_H
+#define QLEARNING_H
+
+#include <math.h>
+#include <time.h>
+#include <mazeEnv.h>
+
+extern float alpha_value; // learning rate 
+extern float gamma_value; // discount factor (importance given to future rewards compared to immediate rewards)
+
+extern int** q;
+extern int** r;
+
+void q_alloc();
+
+void q_init();
+
+int get_state(int col, int row);
+
+action q_update(action a, int state, int reward, int new_state);
+
+action choose_action_epsillon_greedy(int state, double epsillon);
+
+void q_destroy();
+
+void r_init();
+
+void r_destroy();
+
+
+#endif
---
+++ RlMaze/src/qlearning.c
@@ -0,0 +1,191 @@
+#include <qlearning.h>
+
+float alpha_value = 0.1 ; 
+float gamma_value = 0.5 ; 
+int nb_episodes = 1;
+
+
+int** q;
+int** r;
+
+void q_alloc(){
+    q = malloc(rows * cols  * sizeof(double*));
+    action a = number_actions ;
+        for (int i = 0; i < rows * cols ; ++i){
+                q[i] = malloc((int)a * sizeof(double));
+        }
+}
+
+void q_init(){
+        action a = number_actions ;
+        q_alloc();
+        for (int i = 0; i < rows * cols ; ++i) {
+                for (int j = 0; j < (int)a; ++j) {
+                        q[i][j]= 0 ; 
+                }
+        } 
+}
+
+int get_state(int col, int row){
+        return (col * row + col);
+}
+
+action q_update(action a, int state, int reward, int new_state){
+        // return the action that maximize Q
+
+        int action_index = 0 ;
+        double max_val = q[new_state][0];
+
+        for (int i = 0; i < number_actions; i++) {
+                if (q[new_state][i] > max_val) {
+                max_val = q[new_state][i];
+                action_index = i ;
+                }
+        }
+    q[state][a] = q[state][a] + alpha_value * (reward + gamma_value * max_val - q[state][a]);
+    return (enum action) action_index;
+}
+
+action choose_action_epsillon_greedy(int state, double epsilon){
+        double random_number = (double)rand() / RAND_MAX;
+
+    if (random_number > epsilon) {
+        // Exploitation: Choose the action with the highest Q-value for the current state
+        action best_action = 0;
+        double max_q_value = q[state][0];
+        //printf("Le nombre d'action est %d\n", (int)n);
+        for (int a = 0 ; a < number_actions; a++) {
+            if (q[state][a] > max_q_value) {
+                max_q_value = q[state][a];
+                best_action = a;
+            }
+        }
+        printf("L action choisi est celui qui maximise Q \n");
+        return best_action;
+    } else {
+        // Exploration: Choose a random action
+        printf("L action choisi est au hasard\n");
+
+        return env_action_sample();
+    }
+
+}
+
+void q_destroy(){
+    for (int i = 0; i < rows * cols ; ++i){
+                free(q[i]);
+        }
+    free(q);
+}
+
+void r_init(){
+    r = malloc(rows * sizeof(int*));
+        for (int i = 0; i < rows; ++i){
+                r[i] = malloc(cols * sizeof(int));
+        }
+        printf("Le nombre de col %d \n", cols);
+        for (int i = 0; i < rows; ++i) {
+                for (int j = 0; j < cols; ++j) {
+                        if (mazeEnv[i][j] == '+') {
+                                r[i][j] = -2;
+                        } 
+                        else if (mazeEnv[i][j] == 'g') {
+                                r[i][j] = 100;
+                        } 
+                        else if (mazeEnv[i][j] == 's') {
+                                r[i][j] = -1;
+                        } 
+                        else {
+                                r[i][j] = 5;
+                        }
+                // pas de segmentation fault ?
+                // printf("La récompense de la case (%d,%d) est : %d \n" ,i,j,r[0][21]); 
+                }
+        }
+}
+
+void r_destroy(){
+    for (int i = 0; i < rows; ++i){
+                free(r[i]);
+        }
+    free(r);
+}
+
+int main(){
+
+    mazeEnv_make("../data/maze.txt");
+    init_visited();
+
+   printf("%d, %d \n", rows, cols);
+   printf("number of actions :  %d \n", number_actions); 
+   mazeEnv_render();
+
+    q_init(); 
+    r_init();
+    
+    
+
+    action state_action ;
+    action new_action ;
+    int state,new_state;
+    
+    
+    for (int episode = 0 ; episode < nb_episodes ; ++episode){
+
+        // Maze reset & initialisation
+        mazeEnv_reset();
+        state = get_state(state_col, start_row);
+
+        // Chosing action
+        state_action = choose_action_epsillon_greedy(state, 0.5);
+
+        envOutput new_state_env ; 
+        new_state_env = mazeEnv_step(state_action);
+
+        new_state = get_state(new_state_env.new_col, new_state_env.new_row);
+        new_action = q_update(state_action, state, new_state_env.reward, new_state);
+
+        // Update state and action
+        state = new_state ;
+        state_action = new_action ;
+
+        update_visited(new_state_env.new_col, new_state_env.new_row);
+
+        mazeEnvepisode_render_pos();
+
+
+        while(new_state_env.done != 1){
+
+            state = get_state(state_col, start_row);
+            state_action = choose_action_epsillon_greedy(state, 0.5);
+
+            envOutput new_state_env ; 
+            new_state_env = mazeEnv_step(state_action);
+            new_state = get_state(new_state_env.new_col, new_state_env.new_row);
+            //new_action = q_update(state_action, state, new_state_env.reward, new_state);
+
+            state = new_state ;
+            state_action = new_action ;
+
+            //update_visited(new_state_env.new_col, new_state_env.new_row);
+            mazeEnvepisode_render_pos();
+
+        }
+        
+        add_crumbs();
+        
+    }
+    
+
+
+    mazeEnv_destroy();
+    visited_destroy();
+
+    
+
+    q_destroy();
+    r_destroy();
+
+
+    return 0 ;
+}
