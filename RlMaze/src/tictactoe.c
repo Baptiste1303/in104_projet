b30c4029e74fe7f0ba2488aff93f08752a8bc9b6
Baptiste1303 <baptiste.martel@sfr.fr>
2023-05-17 23:12:24 +0200

    ajout commentaires + modification afficher()

--- RlMaze/src/tictactoe.c
+++ RlMaze/src/tictactoe.c
@@ -1,5 +1,6 @@
 #include "tictactoe.h"
 
+// Allocate memory and create a blank tic-tac-toe grid
 int *creer_grille() {
   int *grille = malloc(sizeof(int) * 9);
   for (int i = 0; i < 9; ++i) {
@@ -14,34 +15,36 @@ void reset_grid(int *grille){
   }
 }
 
+// Free the mémory
 void free_grille(int* grille){
     free(grille);
 }
 
-void afficher(int *grille) {
-  int j = 0;
-  for (int i = 1; i < 4; ++i) {
-    for (int k = 0; k < 3; ++k) {
-      if (grille[j+k] == 0){
-          printf(".");
-      }
-      else if(grille[j+k] == 1){
-          printf("x");
-      }
-      else if(grille[j+k] == 2){
-          printf("o");
-      }
+// Display the current grid
+void afficher(int*grille){
+    for (int i=0;i<9;++i){
+        if (i==3 || i==6){
+            printf("\n");
+        }
+        if (grille[i]==0){
+            printf(".");
+        }
+        else if (grille[i]==1){
+            printf("x"); // Human
+        }
+        else {
+            printf("o"); // Bot
+        }
     }
     printf("\n");
-    j = j + 3;
-  }
-  printf("\n");
-};
+}
 
+// Place the player's number (1 for human, 2 for bot) on the grid
 void placer(int *grille, int chiffre, int joueur){
     grille[chiffre]=joueur;
 }
 
+// Randomly place a player's number on the grid
 void placer_alea(int* grille, int joueur){
     int chiffre = rand()%9 ;
     while (grille[chiffre] !=0){
@@ -50,6 +53,7 @@ void placer_alea(int* grille, int joueur){
     grille[chiffre] = joueur;
 }
 
+// Detect and return the winner of the game
 int a_gagne(int* grille, int joueur){
     
     int winning_combinations[8][3] = {
@@ -78,39 +82,42 @@ int a_gagne(int* grille, int joueur){
 void play() {
     int *pgrille = creer_grille();
     srand(time(0));
+    // Number of the box (between 0 and 8) entered by the human
     int chiffre_entre = 0;
-
 
     while (a_gagne(pgrille,1) == -1){
         
         afficher(pgrille);
+        // Retrieve the number of the box where the player wants to play
         printf("Donnez la case (chiffre): ");
         scanf("%d", &chiffre_entre);
 
+        // If the number is not valid
         while (chiffre_entre < 0 || chiffre_entre > 8 || pgrille[chiffre_entre] != 0){
             printf("Donnez une case valide: ");
             scanf("%d", &chiffre_entre);
         }
         
+        // Place the player's token
         placer(pgrille, chiffre_entre, 1);
 
         if(a_gagne(pgrille,1) != -1)
             break;
         
+        //place the robot's token
         placer_alea(pgrille, 2);
     }
 
     afficher(pgrille);
 
+    // Display the winner
     int resultat = a_gagne(pgrille,1);
     if (resultat== 1 ){
-printf("le gagnant est le joueur humain \n");
-}
-else if (resultat == 2){
-printf("le gagnant est le robot \n");
-}
-else {
-printf("Match nul\n");
+        printf("Le gagnant est le joueur humain \n");
+    } else if (resultat == 2){
+        printf("Le gagnant est le robot \n");
+    } else {
+        printf("Match nul\n");
 }
 
 
@@ -132,6 +139,6 @@ printf("La base 10 est %9.d et le chiffre est %d", a,s);
 
 */
 
-free(pgrille);
+    free(pgrille);
 }
 
